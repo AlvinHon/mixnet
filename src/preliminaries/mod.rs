537c126e2919d7e5c8ebdf496ed6ad7f93b3e986
AlvinHon <alvin.cpp@gmail.com>
2026-04-06 20:58:05 +0800

    Remove unneeded code

--- src/preliminaries/mod.rs
+++ src/preliminaries/mod.rs
@@ -1,9 +1,2 @@
 pub mod algebra;
-pub mod errors;
 pub mod mat;
-pub mod params;
-
-pub use algebra::{int_to_bin, is_bin, poly_from_int_bin};
-pub use errors::{MixnetError, MixnetResult};
-pub use params::SecurityParams;
-pub use poly_ring_xnp1::Polynomial;
